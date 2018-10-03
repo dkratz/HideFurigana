ead020dd28b45f731e114f6aacc2757b799c4dad
Ahsous <info@ahsous.net>
2018-10-03 17:00:42 +0200

    added kanji sets, kanji sets level

--- src/options.js
+++ src/options.js
@@ -1,15 +1,40 @@
+function getCheckedCheckboxesFor(checkboxName) {
+    var checkboxes = document.querySelectorAll('input[name="' + checkboxName + '"]:checked'), values = [];
+    Array.prototype.forEach.call(checkboxes, function (el) {
+        values.push(el.value);
+    });
+    return values;
+}
+
+var log = chrome.extension.getBackgroundPage().console.log;
+
 function loadOptions() {
-    chrome.storage.sync.get({ kklcLevel: 0, additionalKanji: "" }, function (data) {
-        document.getElementById("kklcLevel").value = data.kklcLevel;
+    log("loadOptions");
+    chrome.storage.sync.get({ additionalKanji: "", kanjiSets: [], kanjiSetsLevel: {} }, function (data) {
+        log(data);
         document.getElementById("additionalKanji").value = data.additionalKanji;
+        //document.getElementById("kklcLevel").value = data.kklcLevel;
+        for(let i = 0; i < data.kanjiSets.length; i++) {
+            document.querySelector("input[name='kanjiSets'][value='" + data.kanjiSets[i] + "']").checked = true;
+        }
+        for(key in data.kanjiSetsLevel) {
+            document.querySelector(".kanjiSetsLevel[data-name='" + key + "']").value = data.kanjiSetsLevel[key];
+        }
     });
 }
 
 function saveOptions() {
-    console.log("save")
-    let kklcLevel = document.getElementById("kklcLevel").value;
-    let additionalKanji = document.getElementById("additionalKanji").value
-    chrome.storage.sync.set({ kklcLevel: kklcLevel, additionalKanji: additionalKanji }, function () {
+    log("saveOptions");
+    let additionalKanji = document.getElementById("additionalKanji").value;
+    let kanjiSets = getCheckedCheckboxesFor("kanjiSets");
+    let kanjiSetsLevel = {};
+    let nodes = document.querySelectorAll(".kanjiSetsLevel");
+    for(let i = 0; i < nodes.length; i++) {
+        kanjiSetsLevel[nodes[i].dataset.name] = nodes[i].value;
+    }
+    let options = { additionalKanji: additionalKanji, kanjiSets: kanjiSets, kanjiSetsLevel: kanjiSetsLevel };
+    chrome.storage.sync.set(options, function () {
+        log(options);
         document.getElementById("status").textContent = "Options saved."
     });
 }
